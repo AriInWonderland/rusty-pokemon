e5195e14af9eebfcde9fad98bf480ece4579a70b
Alice <alicewulfric@gmail.com>
2024-01-04 14:11:00 -0300

    You can check how money you have now!

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 extern crate byte_string;
 
-use std::{io::{self, Write}, ops::Index};
+use std::{io::{self, Write}, ops::{Index, BitOr, BitOrAssign, BitAndAssign}};
 use io::{BufReader, Read};
 use std::fs::File;
 
 struct Player {
     name: String,
     new_encoded_name: [u8; 11],
+    money: i32,
+    encoded_money: [u8; 6],
 }
 
 struct Game {
@@ -26,24 +28,31 @@ fn main() {
     };
     let mut original_player = Player {
         name: String::new(),
+        money: 0,
         new_encoded_name: [0; 11],
+        encoded_money: [0; 6],
     };
     let mut player = Player {
         name: String::new(),
         new_encoded_name: [0; 11],
+        money: 0, 
+        encoded_money: [0; 6],
     };
 
     'main_loop: loop{
         match main_menu() {
             1 => {
                 load_save_file(&mut game, &mut original_player);
-                player.name = original_player.name.to_string();
+                player.name  = original_player.name.to_string();
+                player.money = original_player.money;
             }
             2 => { 
                 println!("Your name is: {}", player.name);
+                println!("You have ${}", player.money);
             }
             3 => {
                 println!("Your name is: {}", original_player.name);
+                println!("You have ${}", original_player.money);
             }
             4 => {
                 change_player_name(&mut player);
@@ -63,6 +72,23 @@ fn main() {
     }
 }
 
+fn get_money(game_struct: &Game) -> i32{
+    let mut money: i32 = 0;
+    let mut stack: [u8; 6] = [0, 0, 0, 0, 0, 0];
+
+    stack[0].bitor_assign(game_struct.save_file_buffer[0x25f3] >> 4);
+    stack[1].bitor_assign(game_struct.save_file_buffer[0x25f3] & 0b00001111);
+    stack[2].bitor_assign(game_struct.save_file_buffer[0x25f4] >> 4);
+    stack[3].bitor_assign(game_struct.save_file_buffer[0x25f4] & 0b00001111);
+    stack[4].bitor_assign(game_struct.save_file_buffer[0x25f5] >> 4);
+    stack[5].bitor_assign(game_struct.save_file_buffer[0x25f5] & 0b00001111);
+
+    for i in 0..=5{
+        money = money * 10 + stack[i] as i32;
+    }
+
+    return money;
+}
 fn change_player_name(player_struct: &mut Player){
     let mut new_name = String::new();
     let new_encoded_name: [u8; 11];
@@ -94,6 +120,7 @@ fn load_save_file(game_struct: &mut Game, player_struct: &mut Player){
     game_struct.save_file_path = file_path;
     game_struct.save_file_buffer = create_save_file_buffer(&game_struct.save_file_path);
     player_struct.name = get_player_name(&game_struct.save_file_buffer);
+    player_struct.money = get_money(game_struct);
 }
 
 fn main_menu() -> i32 {
@@ -105,7 +132,6 @@ fn main_menu() -> i32 {
     println!("╠══════════════════════════════════╣");
     println!("║ 4. Change my name                ║");
     println!("║ 5. Change my money(WIP)          ║");
-    println!("║ 6. Change my inventory (not yet) ║");
     println!("╠══════════════════════════════════╣");
     println!("║ 7. Change my rival's name(WIP)   ║");
     println!("╠══════════════════════════════════╣");
