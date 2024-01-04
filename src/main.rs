83f46c57e879d0fe01179d8156f2006b5c93edf2
Alice <alicewulfric@gmail.com>
2024-01-04 12:38:41 -0300

    You can encode and decode a name!

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 extern crate byte_string;
 
-use std::io::{self, Write};
+use std::{io::{self, Write}, ops::Index};
 use io::{BufReader, Read};
 use std::fs::File;
 
 struct Player {
     name: String,
+    new_encoded_name: [u8; 11],
 }
 
 struct Game {
@@ -23,21 +24,29 @@ fn main() {
         save_file_path: String::new(),
         save_file_buffer: Vec::new(),
     };
+    let mut original_player = Player {
+        name: String::new(),
+        new_encoded_name: [0; 11],
+    };
     let mut player = Player {
         name: String::new(),
+        new_encoded_name: [0; 11],
     };
 
     'main_loop: loop{
         match main_menu() {
             1 => {
-                load_save_file(&mut game, &mut player);
+                load_save_file(&mut game, &mut original_player);
+                player.name = original_player.name.to_string();
             }
             2 => { 
                 println!("Your name is: {}", player.name);
             }
             3 => {
+                println!("Your name is: {}", original_player.name);
             }
             4 => {
+                change_player_name(&mut player);
             }
             5 => {
             }
@@ -45,11 +54,32 @@ fn main() {
             }
             7 => {
             }
+            8 => {
+            }
             _ => {
                 break 'main_loop;
             }
         };
     }
+}
+
+fn change_player_name(player_struct: &mut Player){
+    let mut new_name = String::new();
+    let new_encoded_name: [u8; 11];
+
+    print!("What will be your new name? ");
+    io::stdout().flush().expect("Failed to flush stdout...");
+    io::stdin()
+        .read_line(&mut new_name)
+        .expect("Failed to read user input");
+
+    new_encoded_name = ascii_to_first_gen(&new_name);
+
+    println!("Encoded name: {:?}", new_encoded_name);
+    println!("ASCII: {:?}", first_gen_to_ascii(new_encoded_name));
+
+    player_struct.name = new_name;
+    player_struct.new_encoded_name = new_encoded_name;
 }
 
 fn load_save_file(game_struct: &mut Game, player_struct: &mut Player){
@@ -71,14 +101,16 @@ fn main_menu() -> i32 {
     println!("╔══════════════════════════════════╗");
     println!("║ 1. Load save file                ║");
     println!("║ 2. Print stats (Buffered)        ║");
-    println!("║ 3. Print stats (From file)       ║");
+    println!("║ 3. Print stats (From file)(WIP)  ║");
     println!("╠══════════════════════════════════╣");
     println!("║ 4. Change my name                ║");
-    println!("║ 5. Change my money               ║");
+    println!("║ 5. Change my money(WIP)          ║");
     println!("║ 6. Change my inventory (not yet) ║");
     println!("╠══════════════════════════════════╣");
-    println!("║ 7. Change my rival's name        ║");
+    println!("║ 7. Change my rival's name(WIP)   ║");
     println!("╠══════════════════════════════════╣");
+    println!("║ 8. Write my changes(WIP)         ║");
+    println!("║ 9. Write and exit(WIP)           ║");
     println!("║ 0. Exit                          ║");
     println!("╚══════════════════════════════════╝");
     print!("--> ");
@@ -102,6 +134,36 @@ fn get_player_name(file_buffer: &Vec<u8>) -> String {
         }
     }
     first_gen_to_ascii(encoded_name)
+}
+
+fn ascii_to_first_gen(ascii_string: &String) -> [u8; 11]{
+    let mut encoded_text: [u8; 11] = [0; 11];
+    let mut index = 0;
+
+    for c in ascii_string.chars() {
+        if c >= 'A' && c <= 'Z' || c >= 'a' && c <= 'z' {
+            encoded_text[index] = ascii_string.as_bytes()[index] + 63;
+        } else {
+            match c {
+                '(' => encoded_text[index] = 0x9A,
+                ')' => encoded_text[index] = 0x9B,
+                ':' => encoded_text[index] = 0x9C,
+                ';' => encoded_text[index] = 0x9D,
+                '[' => encoded_text[index] = 0x9E,
+                ']' => encoded_text[index] = 0x9F,
+                'é' => encoded_text[index] = 0xBA,
+                'ď' => encoded_text[index] = 0xBB,
+                'ľ' => encoded_text[index] = 0xBC,
+                'ś' => encoded_text[index] = 0xBD,
+                'ť' => encoded_text[index] = 0xBE,
+                'ú' => encoded_text[index] = 0xBF,
+                _ => encoded_text[index] = 0x00,
+            };
+        }
+        index += 1;
+    }
+
+    return encoded_text;
 }
 
 fn first_gen_to_ascii(pokemon_encoded: [u8; 11]) -> String{
